b62e3224b771805dec36e68e64d646610251d99e
Mark Vickery <vickerym3@winthrop.edu>
2016-02-09 10:51:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+technoCampLogin
+===============
+
+A Symfony project created on February 8, 2016, 10:57 am.
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+//Our stuff
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Info
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\InfoRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+     
+    protected $id;
+	
+	public function __construct()
+	{
+		parent::__construct();
+	} 
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=50, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=50, nullable=true)
+     */
+    private $lastName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Info
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Info
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+}
